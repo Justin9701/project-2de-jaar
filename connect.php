ea8279250a5fed56b1ed32e4c5428e06be01b8d4
Justin9701 <126084062+Justin9701@users.noreply.github.com>
2023-12-15 11:21:21 +0100

    changes

--- connect.php
+++ connect.php
@@ -13,7 +13,7 @@
 
   //connection sting en PDO settings
   try {
-    $pdo = new PDO("mysql:host=$servername;dbname=coral yachten", $username, $password);
+    $pdo = new PDO("mysql:host=$servername;dbname=coral_jachten", $username, $password);
     // set the PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
